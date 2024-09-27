b450daf2c226fff1705851ab9c8d584f13720c38
“[halil <halilprofil@gmail.com>
2024-09-27 15:56:40 +0300

    update for vercel

--- app/student/[id]/page.jsx
+++ app/student/[id]/page.jsx
@@ -1,13 +1,25 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 
-export default function Student({params}){
-    const {id} = params
+export default function Student({ params }) {
+  const { id } = params;
+  const [student, setStudent] = useState(null);
+
+  useEffect(() => {
     const data = JSON.parse(localStorage.getItem("savedStudents"));
-    const student = data.find(x => x.id == id);
-    return(
-        <>
-        <p>{student.name}</p>
-        <p>{student.absenceStatus}</p>
-        </>
-    )
+    const foundStudent = data?.find(x => x.id == id);
+    setStudent(foundStudent);
+  }, [id]);
+
+  if (!student) {
+    return <p>Öğrenci bilgisi bulunamadı.</p>;
+  }
+
+  return (
+    <>
+      <p>{student.name}</p>
+      <p>{student.absenceStatus}</p>
+    </>
+  );
 }
+
